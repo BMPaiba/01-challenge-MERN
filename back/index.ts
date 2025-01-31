db16469291daec891009d75d1d49ef3e7f54ce14
brian-paiba <brianmanuelpaiba@outlook.es>
2025-01-31 17:51:09 -0300

    feat(db): establish connection to MongoDB using mongoose

--- back/index.ts
+++ back/index.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import mongoose from "mongoose";
 import apiRoutes from "./routes";
+import dotenv from "dotenv";
+
+// Cargar las variables de entorno
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -8,12 +13,30 @@ const port = 3000;
 app.use(morgan("dev"));
 app.use(express.json());
 
+const connectDB = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGODB_URL || "")
+    .then(() => console.log("Connected to Mongo DB"))
+    .catch((err) => {
+      console.error("failed to connect with mongo");
+      console.error(err);
+    });
+};
+
 app.get("/api", (req, res) => {
   res.send("<h1>API is running</h1>");
 });
 
 app.use("/api", apiRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+const startServer = async () => {
+  try {
+    connectDB();
+    app.listen(port, () => console.log(`Server running on port:${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
