bde1a0d2ece5811e9a19c374e5f92ffd7d222b82
brian-paiba <brianmanuelpaiba@outlook.es>
2025-01-31 15:23:26 -0300

    feat(employees): configure routes for employees module

--- back/employees/employees.controller.ts
+++ back/employees/employees.controller.ts
@@ -2,15 +2,11 @@ import type { Request, Response } from "express";
 import { EmployeesService } from "./employees.service";
 
 export class EmployeesController {
-  static login(req: Request, res: Response) {
+
+  static getAllEmployees(req: Request, res: Response) {
     const { email, password } = req.body;
     const result = EmployeesService.login(email, password);
     res.json(result);
   }
-
-  static register(req: Request, res: Response) {
-    const { email, password } = req.body;
-    const result = EmployeesService.register(email, password);
-    res.json(result);
-  }
+  
 }
