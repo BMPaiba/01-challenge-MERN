51b40a67fb369c7f28c9bdba8193756ed7ddeda3
brian-paiba <brianmanuelpaiba@outlook.es>
2025-02-01 10:00:37 -0300

    feat(router): install and configure React Router

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,21 @@
-
+import { Route, Routes } from "react-router";
+import { AuthLayout } from "@/features/auth/layout/auth-layout";
+import Login from "@/features/auth/pages/login";
+import Register from "@/features/auth/pages/register";
 
 function App() {
-
   return (
-    <>
-     <h1>Hello</h1>
-    </>
-  )
+    <Routes>
+
+      <Route path="*" element={<div>404</div>} />
+
+      <Route path="auth" element={<AuthLayout />}>
+        <Route index element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+
+    </Routes>
+  );
 }
 
-export default App
+export default App;
