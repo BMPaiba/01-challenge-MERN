7ef46e46dcfd2e77f87122bf253d4bc2ca56afe7
brian-paiba <brianmanuelpaiba@outlook.es>
2025-02-01 14:22:32 -0300

    feat(auth): add session verification and redirect after login

--- front/src/features/auth/layout/auth-layout.tsx
+++ front/src/features/auth/layout/auth-layout.tsx
@@ -1,9 +1,45 @@
-import { Outlet } from "react-router";
+import  { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import Cookies from "js-cookie"; // Importamos js-cookie
+import { login } from "../store/authSlice";
+import { RootState } from "@/store/store";
 
 export const AuthLayout = () => {
-    return (
-        <div>
-            <Outlet />
-        </div>
-    );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { isAuthenticated, user } = useSelector((state: RootState) => state.auth);
+
+  console.log({isAuthenticated, user})
+
+  const getJwtFromCookies = () => {
+    return Cookies.get("jwt"); 
+  };
+
+  useEffect(() => {
+    const jwt = getJwtFromCookies();
+
+    if (jwt) {
+      // Si hay JWT, puedes hacer una validación del JWT en el backend aquí si lo deseas
+      // Guardar los datos en Redux si es necesario
+      // Por ejemplo, si tienes un objeto `user` que guardaste previamente:
+      const savedUser = JSON.parse(localStorage.getItem("user") || "{}"); // O usar algún otro método
+      if (savedUser) {
+        dispatch(login(savedUser)); // Despachar al store de Redux
+      }
+    }
+
+    if (isAuthenticated) {
+      // Si no hay JWT o no estamos autenticados, redirigir a login
+      navigate("/", { replace: true });
+    }
+  }, [dispatch, navigate, isAuthenticated]);
+
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
+
