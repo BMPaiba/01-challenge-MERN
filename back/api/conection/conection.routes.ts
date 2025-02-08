b0f967473f33e138025e0b99d2d503da5d4d9a36
brian-paiba <brianmanuelpaiba@outlook.es>
2025-02-08 16:43:32 -0300

    fix(conection) add routes

--- back/api/conection/conection.routes.ts
+++ back/api/conection/conection.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { AuthController } from "./auth.controller";
+import { AuthController } from "./conection.controller";
 
 const router = Router();
 
--- back/routes.ts
+++ back/routes.ts
@@ -4,6 +4,7 @@ import employeesRoutes from "./api/employees/employees.routes";
 import evaluationsRoutes from "./api/evaluations/evaluations.routes";
 import feedbackRoutes from "./api/feedback/feedback.routes";
 import reportsRoutes from "./api/reports/reports.routes";
+import conectdbRoutes from "./api/conection/conection.routes";
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use("/employees", employeesRoutes);
 router.use("/evaluations", evaluationsRoutes);
 router.use("/feedback", feedbackRoutes);
 router.use("/reports", reportsRoutes);
+router.use("/conection", conectdbRoutes);
 
 export default router;
